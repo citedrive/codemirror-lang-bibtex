4ebb6ae3aac1fbbf2d0f3b631caa0ba9cfc19f21
vai <vai.sriv@icloud.com>
2025-06-10 17:21:50 -0700

    add boost to recommended

--- src/snippets.ts
+++ src/snippets.ts
@@ -1,28 +1,40 @@
 import {snippet} from "@codemirror/autocomplete"
 
 // funcs for dynamic snippet creation
-export const createEntry = (label: string, section: string, detail: string, fields: string[]) => ({
-    // matching options
-    label: `@${label}`,
-    detail: detail,
+export const createEntry = (label: string, section: string, detail: string, fields: string[]) => {
+    let boost = (section === "Recommended" ? 99 : -99);
+    console.log(section);
+    console.log(boost);
+    return ({
+        // matching options
+        label: `@${label}`,
+        detail: detail,
 
-    //expansion
-    apply: snippet(`@${label}{#{<citationkey>},${fields.map(f => `\n\t${f} = {#{<${f}>}}`)}\n}`),
+        //expansion
+        apply: snippet(`@${label}{#{<citationkey>},${fields.map(f => `\n\t${f} = {#{<${f}>}}`)}\n}`),
 
-    // render options
-    section: section,
-    type: "class"
-});
+        // render options
+        section: section,
+        boost: boost,
+        type: "class"
+    })
+};
 
-export const createField = (label: string, section: string, detail: string) => ({
-    // matching options
-    label: label,
-    detail: detail,
+export const createField = (label: string, section: string, detail: string) => {
+    let boost = (section === "Recommended" ? 99 : -99);
+    console.log(section);
+    console.log(boost);
+    return ({
+        // matching options
+        label: label,
+        detail: detail,
 
-    // expansion
-    apply: snippet(`${label} = {#{<${label}>}}#{,}\n#{}`),
+        // expansion
+        apply: snippet(`${label} = {#{<${label}>}}#{,}\n#{}`),
 
-    // render options
-    section: section,
-    type: "property",
-});
+        // render options
+        section: section,
+        boost: boost,
+        type: "property",
+    })
+};
